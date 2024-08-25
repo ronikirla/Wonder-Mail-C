8774227db518ad8c4e2844aee95f07e52384d0cf
Roni Kirla <roni.kirla@gmail.com>
2024-08-25 22:03:33 +0300

    Working base generator

--- src/generator.c
+++ src/generator.c
@@ -1,8 +1,11 @@
 #include "include.h"
 #include "precompute.h"
+#include "generator.h"
 
-#define BITSTREAM_LEN 138
+#define BITSTREAM_NO_CHECKSUM_LEN 138
+#define BITSTREAM_FULL_LEN 170
 #define NUM_BLOCKS 17
+#define CODE_LEN 34
 
 uint32_t CalculateChecksum(char* bitstream) {
     // Start with 0xFFFFFFFF.
@@ -12,7 +15,7 @@ uint32_t CalculateChecksum(char* bitstream) {
     for (int i = 16; i >= 0; i--) {
         // Grab 8 bits from the stream and convert it to a number.
         char* bit_ptr = bitstream + i * 8 + 2;
-        char substring[8];
+        char substring[8 + 1] = "";
         strncpy(substring, bit_ptr, 8);
         uint8_t num = strtol(substring, NULL, 2);
         
@@ -30,30 +33,38 @@ uint32_t CalculateChecksum(char* bitstream) {
     return checksum;
 }
 
-uint8_t GetResetByte(uint32_t checksum) {
-    uint8_t checksumByte = checksum % 256;
-    uint8_t resetByte = (checksumByte / 16) + 8 + (checksumByte % 16);
+int GetResetByte(uint32_t checksum) {
+    uint8_t checksum_byte = checksum % 256;
+    uint8_t reset_byte = (checksum_byte / 16) + 8 + (checksum_byte % 16);
     // The resetByte must be under 17. If not, the code doesn't use a resetByte.
-    return (resetByte < 17) ? resetByte : -1;
+    return (reset_byte < 17) ? reset_byte : -1;
 }
 
-char* EncryptBitStream(char* bitstream) {
-    char* bit_ptr = bitstream + BITSTREAM_LEN;
+void NumToBits(uint32_t num, int output_size, char* dest) {
+    int index = output_size - 1;
+    while (index >= 0) {
+        dest[index] = (num % 2) + '0';
+        num /= 2;
+        index--;
+    }
+}
+
+void EncryptBitStream(char* bitstream) {
+    char* bit_ptr = bitstream + BITSTREAM_NO_CHECKSUM_LEN;
 
     // This will contain the 8-bit blocks as numbers (0-255), each representing one byte.
     // The checksum byte is NOT included in these blocks.
     // The first block in the array is the last block in the bitstream (we work backwards).
-    int blocks[NUM_BLOCKS];
+    uint8_t blocks[NUM_BLOCKS];
     for (int i = 0; i < NUM_BLOCKS - 1; i++) {
         bit_ptr -= 8;
-        char substring[8];
+        char substring[8 + 1] = "";
         strncpy(substring, bit_ptr, 8);
         int data = strtol(substring, NULL, 2);
         blocks[i] = data;
-        i++;
     }
     bit_ptr -= 2;
-    char substring[10];
+    char substring[10 + 1];
     strncpy(substring, bit_ptr, 10);
     blocks[NUM_BLOCKS - 1] = strtol(substring, NULL, 2);
 
@@ -63,12 +74,117 @@ char* EncryptBitStream(char* bitstream) {
     uint8_t* entries = precompute.encryption_tables.rows[checksum % 256];
 
     // Figure out the resetByte.
-    uint8_t resetByte = GetResetByte(checksum);
+    int reset_byte = GetResetByte(checksum);
 
     // Do the encryption.
+    int enc_idx = 0;
+    for (int i = 0; i < NUM_BLOCKS; i++) {
+        int pos = i * 8;
+        if (enc_idx == reset_byte) {
+            enc_idx = 0;
+        }
 
+        int input_byte = blocks[i];
+
+        // Add the number in the encryption entry to the input byte.
+        bool last = i == NUM_BLOCKS - 1;
+        int mask = last ? 0x3FF : 0xFF;
+        int result = (input_byte + entries[enc_idx]) & mask;
+        // printf("pos %d, value %d, encbyte %d, result is %d\n", i, input_byte, entries[enc_idx], result);
+
+        // Update the data in the block.
+        blocks[i] = result;
+
+        enc_idx++;
+    }
+
+    // Encrypt the input string in place
+    char* encryption_ptr = bitstream;
+    for (int i = NUM_BLOCKS - 1; i >= 0; i--) {
+        if (i == NUM_BLOCKS - 1) {
+            NumToBits(blocks[i], 10, encryption_ptr);
+            encryption_ptr += 10;
+        } else {
+            NumToBits(blocks[i], 8, encryption_ptr);
+            encryption_ptr += 8;
+        }
+    }
+    
+    // Append the checksum
+    NumToBits(checksum, 32, encryption_ptr);
 }
 
-char* GenerateCode(char* bitstream) {
+void BitsToChars(char* bitstream, char* dest) {
+    const char* bit_values = "&67NPR89F0+#STXY45MCHJ-K12=%3Q@W";
+    for (int i = 0; i < CODE_LEN; i++) {
+        char substring[5 + 1] = "";
+        strncpy(substring, bitstream + (CODE_LEN - i - 1) * 5, 5);
+        uint8_t num = strtol(substring, NULL, 2);
+        if (num >= 0 && num < 32) {
+            dest[i] = bit_values[num];
+        } else {
+            fprintf(stderr, "Bad character value: %d", num);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 
+void ScrambleString(char* dest, uint8_t* byte_swap) {
+    char orig[CODE_LEN + 1] = "";
+    strncpy(orig, dest, CODE_LEN);
+
+    for (int i = 0; i < CODE_LEN; i++) {
+        int target = byte_swap[i];
+        dest[target] = orig[i];
+    }
+}
+
+uint8_t byte_swap_JP[] = {
+    0x14, 0x00, 0x13, 0x16, 0x05, 0x12, 0x02, 0x0B,
+    0x0C, 0x19, 0x21, 0x0F, 0x08, 0x1D, 0x11, 0x1A,
+    0x06, 0x01, 0x17, 0x1C, 0x07, 0x1B, 0x0D, 0x1F,
+    0x15, 0x09, 0x1E, 0x0A, 0x20, 0x10, 0x0E, 0x04,
+    0x03, 0x18
+};
+
+uint8_t byte_swap_NA[] = {
+    0x07, 0x1B, 0x0D, 0x1F, 0x15, 0x1A, 0x06, 0x01,
+    0x17, 0x1C, 0x09, 0x1E, 0x0A, 0x20, 0x10, 0x21,
+    0x0F, 0x08, 0x1D, 0x11, 0x14, 0x00, 0x13, 0x16,
+    0x05, 0x12, 0x0E, 0x04, 0x03, 0x18, 0x02, 0x0B,
+    0x0C, 0x19
+};
+
+uint8_t byte_swap_EU[] = {
+    0x0E, 0x04, 0x03, 0x18, 0x09, 0x1E, 0x0A, 0x20,
+    0x10, 0x21, 0x14, 0x00, 0x13, 0x16, 0x05, 0x12,
+    0x06, 0x01, 0x17, 0x1C, 0x07, 0x1B, 0x0D, 0x1F,
+    0x15, 0x1A, 0x02, 0x0B, 0x0C, 0x19, 0x0F, 0x08,
+    0x1D, 0x11
+};
+
+void GenerateCode(char* bitstream, char* dest, enum region region) {
+    // Encrypt the code.
+    EncryptBitStream(bitstream);
+
+    // Bitpack it.
+    BitsToChars(bitstream, dest); 
+
+    // Scramble it.
+    uint8_t* byte_swap;
+    switch(region) {
+        case REGION_JP:
+            byte_swap = byte_swap_JP;
+            break;
+        case REGION_NA:
+            byte_swap = byte_swap_NA;
+            break;
+        case REGION_EU:
+            byte_swap = byte_swap_EU;
+            break;
+        default:
+            byte_swap = byte_swap_NA;
+    }
+
+    ScrambleString(dest, byte_swap);
 }
--- src/generator.h
+++ src/generator.h
@@ -1,1 +1,9 @@
-char* GenerateCode(char* bitstream);
+#pragma once
+
+enum region {
+    REGION_JP = 0,
+    REGION_NA = 1,
+    REGION_EU = 2
+};
+
+void GenerateCode(char* bitstream, char* dest, enum region region);
